6596a67ac1650cca89e337d43596d68169649cdc
iaugspach <iaugspach97@gmail.com>
2021-05-06 18:35:18 -0300

    isa post

--- Router/userRoutes.js
+++ Router/userRoutes.js
@@ -1,10 +1,31 @@
 const express           = require('express');
 const router            = express.Router()
 const userController    = require('../controllers/userController');
+const path = require ('path');
+const { body } = require('express-validator');
 
 console.log('entro en rutas de usuario');
 
+const validations = [
+    body('name').notEmpty() .withMessage('Completá el nombre'),
+    body('lastname').notEmpty() .withMessage('Completá el apellido'),
+    body('user').notEmpty() .withMessage('Completá el usuario'),
+    body('dateBrith').notEmpty() .withMessage('Completá la fecha'),
+    body('address').notEmpty() .withMessage('Completá la dirección'),
+    body('numaddress').notEmpty() .withMessage('Completá el número'),
+    body('floor').notEmpty() .withMessage('Completá el piso'),
+    body('city').notEmpty() .withMessage('Completá la ciudad'),
+    body('pass').notEmpty() .withMessage('Completá la contrasena'),
+    body('confirmpass').notEmpty() .withMessage('Completá la contrasena'),
+    body('email').notEmpty() .withMessage('Completá el correo electrónico'). bail ()
+    .isEmail ()  .withMessage('Completá un correo electrónico válido'),
+    body('idfiscal').notEmpty() .withMessage('Completá el ID'),
+];
+
 router.get('/register',userController.register);
+router.post('/register', validations, userController.processRegister);
+
+
 router.get('/login',userController.login);
 
 module.exports = router;
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,44 @@
 console.log('entro en Controlador de usuario');
+const {validationResult} = require('express-validator');
 let userControler = {
-    login:(req,res)=>{    
-        res.render('users/login');
-    },
+    
     register:(req,res)=>{
         console.log('entro en método register');
         res.render('users/register');
+    },
+    processRegister: (req, res) => {
+        const resultValidation = validationResult(req);
+        
+        if (resultValidation.errors.length > 0) {
+            return res.render('userRegisterForm', {
+                errors: resultValidation.mapped(),
+                oldData: req.body(),
+            });
+        }
+        return res.send ('Ok, las validaciones pasaron con éxito!')
+    },        
+        //return res.send(resultValidation);
+        /*let user = {
+            name: req.body.name,
+            lastname: req.body.lastname,
+            user: req.body.user,
+            dateBrith: req.body.dateBrith,
+            address: req.body.address,
+            numaddress: req.body.numaddress,
+            floor: req.body.floor,
+            numFloor: req.body.numFloor,
+            city: req.body.city,
+            email: req.body.email,
+            pass: req.body.pass,
+            confirmpass: req.body.confirmpass,
+            fiscalCondition: req.body.fiscalCondition,
+            idfiscal: req.body.idfiscal,
+        }
+        */
+        //guardarla
+
+    login:(req,res)=>{    
+        res.render('users/login');
     }
 };
 
