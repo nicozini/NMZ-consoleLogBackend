0852505a92f42d507cbf3bc7564511f3be7a25f7
iaugspach <iaugspach97@gmail.com>
2021-07-05 18:56:15 -0300

    isa datos mysql

---
+++ temp/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO `verdumarket-test`.`categories` (`id`, `name`) VALUES ('1', 'Fruta');
+INSERT INTO `verdumarket-test`.`categories` (`id`, `name`) VALUES ('2', 'Verdura');
+INSERT INTO `verdumarket-test`.`categories` (`id`, `name`) VALUES ('3', 'Almacen');
+INSERT INTO `verdumarket-test`.`categories` (`id`, `name`) VALUES ('4', 'Dietetica');
