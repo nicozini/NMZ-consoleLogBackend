03bc4c01b6112cab14aea8b4164607aaf67f57de
nicozini <nicolasmartinzini@outlook.com>
2021-07-22 18:24:46 -0300

    Nico Zini - Update user login validations

--- public/js/loginUserValidation.js
+++ public/js/loginUserValidation.js
@@ -1,59 +1,76 @@
 let errors = {};
 
-let form = document.getElementById('form-login');
+let form = document.querySelector('form.formulario');
 
-const user = document.getElementById('user');
+const email = document.getElementById('email');
 const password = document.getElementById('password');
 
 
+// --- Validaciones --- //
+
+// Email
 let validateEmail = function(){
     let feedback = '';
-    let feedbackElement = user.nextElementSibling;
+    let feedbackElement = email.nextElementSibling;
+    
+    let emailRegex = /[\w._%+-]+@[\w.-]+\.[a-zA-Z]{2,4}/
 
-    if(user.value.trim() == '' ){
+    if(email.value.trim() == '' ){
         feedback = 'El email no puede estar vacío';
-    }
-    //else if(expresión regular) -
-
+    } else if (!emailRegex.test(email.value)) {
+        feedback = 'Email incorrecto. Por favor ingrese un correo electrónico válido.'        
+    }    
+    
     if(feedback){
-        user.classList.add('error-input');
-        errors.user = feedback;
+        email.classList.add('error-input');
+        errors.email = feedback;
     }
     else{
-        user.classList.remove('error-input');
-        //feedbackElement.remove();
-        delete errors.user;
+        email.classList.remove('error-input');
+        delete errors.email;
+    }
+
+    feedbackElement.innerText = feedback;    
+}
+
+// Contraseña
+let validatePassword = function() {
+    let feedback = '';
+    let feedbackElement = password.nextElementSibling;
+
+    let pass = password.value; 
+    
+    // Regular expression: 
+    let regex = /^(?=.*\d)(?=.*[a-záéíóúüñ]).*[A-ZÁÉÍÓÚÜÑ]/;
+    // Un dígito: \d
+    // Una minúscula: [a-záéíóúüñ]
+    // Una mayúscula: [A-ZÁÉÍÓÚÜÑ]
+
+    if(pass.trim() == '' ) {
+        feedback = 'La contraseña no puede estar vacía';
+    } else if (pass.length < 8) {
+        feedback = 'La contraseña debe tener al menos 8 caracteres.'
+    } else if (!regex.test(pass)) {
+        feedback = 'La contraseña debe contener una letra minúscula y una letra mayúscula.'
+    }
+
+    if(feedback){        
+        password.classList.add('error-input'); 
+        errors.password = feedback;
+    }
+    else{
+        password.classList.remove('error-input');
+        delete errors.password;
     }
 
     feedbackElement.innerText = feedback;
 }
 
-let validatePassword = function(){
-    let feedback = '';
-    let feedbackElement = password.nextElementSibling;
 
-    if(password.value.trim() == '' ){
-        feedback = 'El campo contraseña no puede estar vacío';
-    }
-    else if(password.value.length < 8){
-        feedback = 'La contraseña tiene que tener por lo menos 8 caracteres';
-    }
-
-    if(feedback){
-        password.classList.add('error-input');
-        errors.password = feedback;
-    }
-    else{
-        password.classList.remove('error-input');
-        //feedbackElement.remove();
-        delete errors.password;
-    }
-
-    feedbackElement.innerText = feedback;
-    }
-
-user.addEventListener('blur', validateEmail);
+// --- Eventos --- //
+email.addEventListener('blur', validateEmail);
 password.addEventListener('blur', validatePassword);
+
 
 form.addEventListener('submit', function(e){
     validateEmail();
@@ -61,4 +78,4 @@ form.addEventListener('submit', function(e){
     if(Object.keys(errors).length){
         e.preventDefault();
     }
-})
+});
